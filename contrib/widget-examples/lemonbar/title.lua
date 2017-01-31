be39959a0d515d656ae269b4e5adccffed608a1b
Федотов Сергей <fedotov_sv@taximaxim.ru>
2017-01-31 11:24:23 +0500

    fix error if got "nil"

--- contrib/widget-examples/lemonbar/title.lua
+++ contrib/widget-examples/lemonbar/title.lua
@@ -4,7 +4,8 @@ utf8 = require "utf8" -- git@github.com:Stepets/utf8.lua.git
 widget = {
   plugin = 'xtitle',
   cb = function(t)
-    t = luastatus.barlib.escape(t) or ''
+    t = t or ''
+    t = luastatus.barlib.escape(t)
     t = (utf8.len(t) < 100 and t) or utf8.sub(t, 1, 97) .. '...'
     return '%{c}' .. colorize(t, '#ffe') .. '%{r}'
   end,
