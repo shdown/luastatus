cd7f900706b6844059478e2545970fcbee312174
Федотов Сергей <fedotov_sv@taximaxim.ru>
2017-01-31 13:11:44 +0500

    small improvements

--- contrib/widget-examples/lemonbar/lib.lua
+++ contrib/widget-examples/lemonbar/lib.lua
@@ -5,19 +5,19 @@ function colorize(text, fg, bg, u)
   return text
 end
 
-function pager(status)
-  local colors = {
-    M = { fg='#000000', bg='#ff5900' },
-    m = { fg='#ff5900', bg='#000000' },
-    O = { fg='#fdf6e3', bg='#808080' },
-    o = { fg='#fdf6e3', bg='#000000' },
-    F = { fg='#002b36', bg='#808080' },
-    f = { fg='#586e57', bg='#000000' },
-    U = { fg='#000000', bg='#dc322f' },
-    u = { fg='#000000', bg='#dc322f' },
-    L = { fg='#ffffff', bg='#000000' },
-  }
+colors = {
+  M = { fg='#000000', bg='#ff5900' },
+  m = { fg='#ff5900', bg='#000000' },
+  O = { fg='#fdf6e3', bg='#808080' },
+  o = { fg='#fdf6e3', bg='#000000' },
+  F = { fg='#002b36', bg='#808080' },
+  f = { fg='#586e57', bg='#000000' },
+  U = { fg='#000000', bg='#dc322f' },
+  u = { fg='#000000', bg='#dc322f' },
+  L = { fg='#ffffff', bg='#000000' },
+}
 
+function pager(status)
   local text = ''
   for block in status:sub(2):gmatch("[^:]+") do
     text = text .. 
@@ -27,7 +27,6 @@ function pager(status)
         colors[block:sub(1,1)].bg
       )
   end
-
   return text
 end
 
--- contrib/widget-examples/lemonbar/title.lua
+++ contrib/widget-examples/lemonbar/title.lua
@@ -5,8 +5,8 @@ widget = {
   plugin = 'xtitle',
   cb = function(t)
     t = t or ''
-    t = luastatus.barlib.escape(t)
     t = (utf8.len(t) < 100 and t) or utf8.sub(t, 1, 97) .. '...'
+    t = luastatus.barlib.escape(t)
     return '%{c}' .. colorize(t, '#ffe') .. '%{r}'
   end,
 }
