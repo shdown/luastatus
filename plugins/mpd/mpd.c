586f7111bb0db8e9438f883e6016546fa7ad2fcf
shdown <shdownnine@gmail.com>
2017-02-03 23:09:25 +0300

    plugins/mpd/mpd.c: support UNIX domain socket path specified as hostname
    Hopefully fixes #12.

--- plugins/mpd/mpd.c
+++ plugins/mpd/mpd.c
@@ -8,6 +8,8 @@
 
 #include <time.h>
 #include <sys/socket.h>
+#include <sys/un.h>
+
 #include <netdb.h>
 
 #include <sys/select.h>
@@ -98,6 +100,34 @@ error:
     priv_destroy(p);
     free(p);
     return LUASTATUS_PLUGIN_INIT_RESULT_ERR;
+}
+
+int
+socket_open(LuastatusPluginData *pd, const char *path)
+{
+    struct sockaddr_un saun;
+    const size_t npath = strlen(path);
+    if (npath + 1 > sizeof(saun.sun_path)) {
+        LUASTATUS_ERRF(pd, "socket path is too long");
+        return -1;
+    }
+    saun.sun_family = AF_UNIX;
+    memcpy(saun.sun_path, path, npath + 1);
+    int fd = ls_cloexec_socket(PF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0) {
+        LS_WITH_ERRSTR(s, errno,
+            LUASTATUS_ERRF(pd, "socket: %s", s);
+        );
+        return -1;
+    }
+    if (connect(fd, (const struct sockaddr *) &saun, sizeof(saun)) < 0) {
+        LS_WITH_ERRSTR(s, errno,
+            LUASTATUS_ERRF(pd, "connect: %s", s);
+        );
+        ls_close(fd);
+        return -1;
+    }
+    return fd;
 }
 
 int
@@ -428,7 +458,9 @@ run(LuastatusPluginData *pd, LuastatusPluginCallBegin call_begin, LuastatusPlugi
     while (1) {
         report_status(pd, call_begin, call_end, "connecting");
 
-        int fd = tcp_open(pd, p->hostname, portstr);
+        int fd = p->hostname[0] == '/'
+            ? socket_open(pd, p->hostname)
+            : tcp_open(pd, p->hostname, portstr);
         if (fd >= 0) {
             interact(fd, pd, call_begin, call_end);
         }
