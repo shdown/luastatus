1b94ad647702f66a11fbf3b12018a717d1f1fe1c
shdown <shdownnine@gmail.com>
2017-02-27 15:49:22 +0300

    libls/string.h: add comments

--- libls/string.h
+++ libls/string.h
@@ -9,8 +9,11 @@
 #include "vector.h"
 #include "compdep.h"
 
+// LSString is a string defined as an LS_VECTOR of chars. It is guaranteed to be defined like that,
+// so that all the LS_VECTOR_* macros work with LSStrings.
 typedef LS_VECTOR_OF(char) LSString;
 
+// Assigns a string value to an LSString. If nbuf is 0, buf is not required to be dereferencable.
 LS_INHEADER
 void
 ls_string_assign_b(LSString *s, const char *buf, size_t nbuf)
@@ -23,6 +26,7 @@ ls_string_assign_b(LSString *s, const char *buf, size_t nbuf)
     s->size = nbuf;
 }
 
+// Assigns a zero-terminated value to an LSString.
 LS_INHEADER
 void
 ls_string_assign_s(LSString *s, const char *cstr)
@@ -30,6 +34,7 @@ ls_string_assign_s(LSString *s, const char *cstr)
     ls_string_assign_b(s, cstr, strlen(cstr));
 }
 
+// Appends a string to an LSString. If nbuf is 0, buf is not required to be dereferencable.
 LS_INHEADER
 void
 ls_string_append_b(LSString *s, const char *buf, size_t nbuf)
@@ -42,6 +47,7 @@ ls_string_append_b(LSString *s, const char *buf, size_t nbuf)
     s->size += nbuf;
 }
 
+// Appends a zero-terminated string to an LSString.
 LS_INHEADER
 void
 ls_string_append_s(LSString *s, const char *cstr)
@@ -49,6 +55,7 @@ ls_string_append_s(LSString *s, const char *cstr)
     ls_string_append_b(s, cstr, strlen(cstr));
 }
 
+// Appends a char to an LSString.
 LS_INHEADER
 void
 ls_string_append_c(LSString *s, char c)
@@ -56,6 +63,7 @@ ls_string_append_c(LSString *s, char c)
     LS_VECTOR_PUSH(*s, c);
 }
 
+// Assigns a char value to an LSString.
 LS_INHEADER
 void
 ls_string_assign_c(LSString *s, char c)
@@ -63,9 +71,13 @@ ls_string_assign_c(LSString *s, char c)
     ls_string_assign_b(s, &c, 1);
 }
 
+// Appends a formatted string to an LSString.
+// Returns false if an encoding error occurs.
 bool
 ls_string_append_vf(LSString *s, const char *fmt, va_list vl);
 
+// Appends a formatted string to an LSString.
+// Returns false if an encoding error occurs.
 LS_INHEADER LS_ATTR_PRINTF(2, 3)
 bool
 ls_string_append_f(LSString *s, const char *fmt, ...)
@@ -77,6 +89,8 @@ ls_string_append_f(LSString *s, const char *fmt, ...)
     return r;
 }
 
+// Assigns a formatted string value to an LSString.
+// Returns false if an encoding error occurs.
 LS_INHEADER
 bool
 ls_string_assign_vf(LSString *s, const char *fmt, va_list vl)
@@ -85,6 +99,8 @@ ls_string_assign_vf(LSString *s, const char *fmt, va_list vl)
     return ls_string_append_vf(s, fmt, vl);
 }
 
+// Assigns a formatted string value to an LSString.
+// Returns false if an encoding error occurs.
 LS_INHEADER LS_ATTR_PRINTF(2, 3)
 bool
 ls_string_assign_f(LSString *s, const char *fmt, ...)
