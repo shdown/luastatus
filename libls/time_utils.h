44c652d4c9ae7490da60aafe41b837ef5b5eae6f
shdown <shdownnine@gmail.com>
2017-03-04 13:04:09 +0300

    libls/time_utils.h: move some functions to time_utils.c
    Also don't pollute global namespace with TIME_T_LIMIT.

--- libls/time_utils.h
+++ libls/time_utils.h
@@ -4,7 +4,6 @@
 #include <time.h>
 #include <sys/time.h>
 #include <stdbool.h>
-#include <errno.h>
 #include "compdep.h"
 
 static const struct timespec ls_timespec_invalid = {0, -1};
@@ -24,42 +23,10 @@ ls_timeval_is_invalid(struct timeval tv)
     return tv.tv_usec == -1;
 }
 
-static const double TIME_T_LIMIT = 2147483647.;
-
-LS_INHEADER
 struct timespec
-ls_timespec_from_seconds(double seconds)
-{
-    if (seconds < 0) {
-        errno = EDOM;
-        return ls_timespec_invalid;
-    }
-    if (seconds > TIME_T_LIMIT) {
-        errno = ERANGE;
-        return ls_timespec_invalid;
-    }
-    return (struct timespec) {
-        .tv_sec = seconds,
-        .tv_nsec = (seconds - (time_t) seconds) * 1e9,
-    };
-}
+ls_timespec_from_seconds(double seconds);
 
-LS_INHEADER
 struct timeval
-ls_timeval_from_seconds(double seconds)
-{
-    if (seconds < 0) {
-        errno = EDOM;
-        return ls_timeval_invalid;
-    }
-    if (seconds > TIME_T_LIMIT) {
-        errno = ERANGE;
-        return ls_timeval_invalid;
-    }
-    return (struct timeval) {
-        .tv_sec = seconds,
-        .tv_usec = (seconds - (time_t) seconds) * 1e6,
-    };
-}
+ls_timeval_from_seconds(double seconds);
 
 #endif
