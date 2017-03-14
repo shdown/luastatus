73a0be2fd71c041aae5be1492599285a5e49b632
shdown <shdownnine@gmail.com>
2017-03-14 11:07:47 +0300

    repo/qluastatus/contrib/widget-examples/i3/cpu-usage.lua: add

---
+++ contrib/widget-examples/i3/cpu-usage.lua
@@ -0,0 +1,32 @@
+f = assert(io.open('/proc/stat', 'r'))
+f:setvbuf('no')
+
+function getstats()
+    f:seek('set', 0)
+    local t = {}
+    t.user, t.nice, t.system, t.idle, t.iowait, t.irq, t.softirq, t.steal, t.guest, t.guest_nice =
+        string.match(f:read('*line'), 'cpu +(.*) +(.*) +(.*) +(.*) +(.*) +(.*) +(.*) +(.*) +(.*) +(.*)')
+    return t
+end
+
+function get_total_idle(t)
+    local idle = t.idle + t.iowait
+    return idle,
+           idle + t.user + t.nice + t.system + t.irq + t.softirq + t.steal
+end
+
+widget = {
+    plugin = 'timer',
+    opts = {period = 2},
+    cb = function()
+        local cur = getstats()
+        if prev then
+            local ci, ct = get_total_idle(cur)
+            local pi, pt = get_total_idle(prev)
+            local di, dt = ci - pi, ct - pt
+            local perc = (dt - di) / dt * 100
+            return {full_text = string.format('[%3.0f%%]', perc)}
+        end
+        prev = cur
+    end,
+}
