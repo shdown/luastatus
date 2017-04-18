b7403a14a6a21326f221af3414eadda59aafe56b
shdown <shdownnine@gmail.com>
2017-04-18 11:07:26 +0300

    contrib/widget-examples/i3/cpu-usage.lua: make it work

--- contrib/widget-examples/i3/cpu-usage.lua
+++ contrib/widget-examples/i3/cpu-usage.lua
@@ -1,32 +1,47 @@
 f = assert(io.open('/proc/stat', 'r'))
 f:setvbuf('no')
 
-function getstats()
+cur = {}
+prev = nil
+
+function upd_cur()
     f:seek('set', 0)
-    local t = {}
-    t.user, t.nice, t.system, t.idle, t.iowait, t.irq, t.softirq, t.steal, t.guest, t.guest_nice =
+    cur.user, cur.nice, cur.system, cur.idle, cur.iowait, cur.irq, cur.softirq, cur.steal, cur.guest, cur.guest_nice =
         string.match(f:read('*line'), 'cpu +(.*) +(.*) +(.*) +(.*) +(.*) +(.*) +(.*) +(.*) +(.*) +(.*)')
-    return t
 end
 
-function get_idle_total(t)
-    local idle = t.idle + t.iowait
-    return idle,
-           idle + t.user + t.nice + t.system + t.irq + t.softirq + t.steal
+function wrapz(x)
+    return x < 0 and 0 or x
 end
 
 widget = {
     plugin = 'timer',
-    opts = {period = 2},
+    opts = {period = 1},
     cb = function()
-        local cur = getstats()
+        upd_cur()
+        cur.user = cur.user - cur.guest
+        cur.nice = cur.nice - cur.guest_nice
+
+        cur.IdleAll = cur.idle + cur.iowait
+        cur.SysAll = cur.system + cur.irq + cur.softirq
+        cur.VirtAll = cur.guest + cur.guest_nice
+        cur.Total = cur.user + cur.nice + cur.SysAll + cur.IdleAll + cur.steal + cur.VirtAll
+
+        local num, denom = 0, 0
         if prev then
-            local ci, ct = get_idle_total(cur)
-            local pi, pt = get_idle_total(prev)
-            local di, dt = ci - pi, ct - pt
-            local perc = (dt - di) / dt * 100
-            return {full_text = string.format('[%3.0f%%]', perc)}
+            num = wrapz(cur.user - prev.user)
+                + wrapz(cur.nice - prev.nice)
+                + wrapz(cur.SysAll - prev.SysAll)
+                + wrapz(cur.steal - prev.steal)
+                + wrapz(cur.guest - prev.guest)
+            denom = wrapz(cur.Total - prev.Total)
         end
+
         prev = cur
+        cur = {}
+
+        if denom ~= 0 then
+            return {full_text = string.format('[%3.0f%%]', num / denom * 100)}
+        end
     end,
 }
