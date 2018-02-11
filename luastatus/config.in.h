7fea848667d3d0a53552af58941cd194e461fa97
shdown <shdownnine@gmail.com>
2018-02-11 19:52:54 +0300

    luastatus/: fix version identification and hard-coding machinery

--- luastatus/config.in.h
+++ luastatus/config.in.h
@@ -4,6 +4,6 @@
 #define LUASTATUS_POSIX_SH_PATH "@luastatus_POSIX_SH_PATH@"
 #define LUASTATUS_PLUGINS_DIR   "@PLUGINS_DIR@"
 #define LUASTATUS_BARLIBS_DIR   "@BARLIBS_DIR@"
-#define LUASTATUS_VERSION       "@VERSION@"
+#define LUASTATUS_VERSION       "@luastatus_VERSION@"
 
 #endif
