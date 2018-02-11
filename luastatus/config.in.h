1ef5f011b423ed9e149637d16110557b67a7c92f
shdown <shdownnine@gmail.com>
2018-02-11 19:55:51 +0300

    luastatus/: get rid of LUASTATUS_POSIX_SH_PATH
    Isn’t needed anymore.

--- luastatus/config.in.h
+++ luastatus/config.in.h
@@ -1,7 +1,6 @@
 #ifndef config_h_
 #define config_h_
 
-#define LUASTATUS_POSIX_SH_PATH "@luastatus_POSIX_SH_PATH@"
 #define LUASTATUS_PLUGINS_DIR   "@PLUGINS_DIR@"
 #define LUASTATUS_BARLIBS_DIR   "@BARLIBS_DIR@"
 #define LUASTATUS_VERSION       "@luastatus_VERSION@"
