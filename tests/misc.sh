d33300066dee8c610aea6c16f8ac38999ce0c15d
shdown <shdownnine@gmail.com>
2018-08-10 20:53:16 +0300

    test/misc.sh: fix/add more tests

--- tests/misc.sh
+++ tests/misc.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-LUASTATUS=(../luastatus/luastatus ${DEBUG:+-l trace})
-HANG_TIMEOUT=${TIMEOUT:-2}
+(
+    cd ..
+    cmake -DCMAKE_BUILD_TYPE=Debug .
+    make -C luastatus
+    make -C tests
+)
+
+LUASTATUS=(valgrind --error-exitcode=42 ../luastatus/luastatus ${DEBUG:+-l trace})
+HANG_TIMEOUT=${TIMEOUT:-7}
 
 fail()
 {
@@ -52,15 +59,31 @@ assert_works()
     assert_succeeds -e "$@"
 }
 
+assert_works_1W()
+{
+    local tail=${@:$#}
+    local init=("${@:1:$#-1}")
+    assert_hangs "${init[@]}" <(printf '%s\n' "$tail")
+    assert_succeeds -e "${init[@]}" <(printf '%s\n' "$tail")
+}
+
 assert_fails
 assert_fails /dev/null
 assert_fails /dev/nil
 assert_fails -e
 assert_fails -TheseFlagsDoNotExist
+assert_fails -l ''
+assert_fails -l nosuchloglevel
+assert_fails -l '•÷¢˝Q⅓'
 assert_fails -l
 assert_fails -l -l
 assert_fails -ы
 assert_fails -v
+assert_fails -b
+
+assert_fails -b '§n”™°£'
+assert_fails -b '/'
+assert_fails -b 'nosuchbarlibforsure'
 
 B='-b ./barlib-mock.so'
 
@@ -72,9 +95,9 @@ assert_works $B . . . . .
 assert_works $B /dev/null
 assert_works $B /dev/null /dev/null /dev/null
 
-assert_works $B <(echo 'luastatus = nil')
+assert_works_1W $B 'luastatus = nil'
 
-assert_works $B <(cat <<EOF
+assert_works_1W $B '
 widget = setmetatable(
     {
         plugin = setmetatable({}, {
@@ -84,10 +107,61 @@ widget = setmetatable(
     }, {
         __index = function() error("hi there (__index)") end,
         __pairs = function() error("hi there (__pairs)") end,
-    })
+    }
+)'
+
+assert_works_1W $B 'widget = {plugin = "./plugin-mock.so", cb = function() end}'
+
+assert_exits_with_code 134 $B <(cat <<EOF
+widget = {
+    plugin = "./plugin-mock.so",
+    opts = { make_calls = 1 },
+    cb = function() luastatus.map_get_handle("some-innocent-key") end,
+}
 EOF
 )
 
-assert_works $B <(echo 'widget = {plugin = "./plugin-mock.so", cb = function() end}')
+assert_works_1W $B '
+h = luastatus.map_get_handle("some-innocent-key")
+
+function show(x)
+    if type(x) == "string" then
+        return "\"" .. x .. "\""
+    else
+        return tostring(x)
+    end
+end
+
+function check(expected, exitcode)
+    local found = h:read()
+    if expected ~= found then
+        print("FAIL: h:read() returned " .. show(found) .. " when expected " .. show(expected))
+        os.exit(exitcode)
+    end
+end
+
+check(nil, 88)
+
+h:write()
+check(nil, 89)
+
+h:write("test")
+check("test", 90)
+
+h:write()
+check(nil, 91)
+
+i = 0
+h:write(tostring(i))
+
+widget = {
+    plugin = "./plugin-mock.so",
+    opts = { make_calls = 1000 },
+    cb = function()
+        check(tostring(i), 92)
+        i = i + 1
+        h:write(tostring(i))
+    end,
+}'
 
 echo >&2 "=== PASSED ==="
