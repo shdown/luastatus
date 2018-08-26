ef87dc89f80c74b76f2fc54b566280d1474d06a1
Yevgeniy Filatov <itspec.ru@gmail.com>
2018-08-26 19:36:54 +0400

    contrib/widget-examples/{dwm,i3}/xkb.lua some improvement

--- contrib/widget-examples/dwm/xkb.lua
+++ contrib/widget-examples/dwm/xkb.lua
@@ -1,12 +1,14 @@
 widget = {
     plugin = 'xkb',
     cb = function(t)
-        if t.name == 'us' then
+        local reg = '([^%(]+)'
+        local base_layout = string.match(t.name, reg)
+        if (base_layout == 'gb' or base_layout == 'us') then
             return '[En]'
-        elseif t.name == 'ru(winkeys)' then
+        elseif (base_layout == 'ru') then
             return '[Ru]'
         else
-            return '[' .. (t.name or '? ID ' .. t.id) .. ']'
+            return '[' .. (base_layout:sub(1,1):upper()..base_layout:sub(2) or '? ID ' .. t.id) .. ']'
         end
     end,
 }
--- contrib/widget-examples/i3/xkb.lua
+++ contrib/widget-examples/i3/xkb.lua
@@ -1,12 +1,14 @@
 widget = {
     plugin = 'xkb',
     cb = function(t)
-        if t.name == 'us' then
+        local reg = '([^%(]+)'
+        local base_layout = string.match(t.name, reg)
+        if (base_layout == 'gb' or base_layout == 'us') then
             return {full_text = '[En]', color = '#9c9c9c'}
-        elseif t.name == 'ru(winkeys)' then
+        elseif (base_layout == 'ru') then
             return {full_text = '[Ru]', color = '#eab93d'}
         else
-            return {full_text = '[' .. (t.name or '? ID ' .. t.id) .. ']'}
+            return {full_text = '[' .. (base_layout:sub(1,1):upper()..base_layout:sub(2) or '? ID ' .. t.id) .. ']'}
         end
     end,
 }
