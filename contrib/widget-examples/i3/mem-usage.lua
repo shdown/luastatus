c56a9c8aa6cb6ed37c710f6ecb29bd8ac03d9e19
Yevgeniy Filatov <itspec.ru@gmail.com>
2018-08-26 22:00:44 +0400

    contrib/widget-examples/i3/mem-usage.lua fix

--- contrib/widget-examples/i3/mem-usage.lua
+++ contrib/widget-examples/i3/mem-usage.lua
@@ -2,6 +2,6 @@ widget = luastatus.require_plugin('mem-usage-linux').widget{
     timer_opts = {period = 2},
     cb = function(t)
         local used_kb = t.total.value - t.avail.value
-        return {full_text = string.format('[%3.2f %s]', used_kb / 1024 / 1024), color = '#af8ec3'}
+        return {full_text = string.format('[%3.2f GiB]', used_kb / 1024 / 1024), color = '#af8ec3'}
     end,
 }
