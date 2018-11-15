cbe50501fab46f6ec738a3a2c2aa229fb7cbefbb
shdown <shdownnine@gmail.com>
2018-11-15 21:58:10 +0300

    generate-man.sh: try rst2man.py as well

--- generate-man.sh
+++ generate-man.sh
@@ -5,4 +5,17 @@ if [ "$#" -ne 2 ]; then
     exit 2
 fi
 
-sed 's/^\.\. :X-man-page-only: \?//' -- "$1" | rst2man > "$2"
+has() {
+    command -v "$1" >/dev/null
+}
+
+if has rst2man; then
+    RST2MAN=rst2man
+elif has rst2man.py; then
+    RST2MAN=rst2man.py
+else
+    echo >&2 "ERROR: neither 'rst2man' nor 'rst2man.py' were found."
+    exit 1
+fi
+
+sed 's/^\.\. :X-man-page-only: \?//' -- "$1" | $RST2MAN > "$2"
