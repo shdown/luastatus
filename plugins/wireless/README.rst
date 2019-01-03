d2f59c54de061617d6a00b04f85547effffa1efb
shdown <shdownnine@gmail.com>
2019-01-03 14:58:10 +0300

    plugins/wireless: add

---
+++ plugins/wireless/README.rst
@@ -0,0 +1,62 @@
+.. :X-man-page-only: luastatus-plugin-wireless
+.. :X-man-page-only: #########################
+.. :X-man-page-only:
+.. :X-man-page-only: #############################################
+.. :X-man-page-only: Wireless connection info plugin for luastatus
+.. :X-man-page-only: #############################################
+.. :X-man-page-only:
+.. :X-man-page-only: :Copyright: LGPLv3
+.. :X-man-page-only: :Manual section: 7
+
+Overview
+========
+This plugin reports the information on a wireless (Wi-Fi) connection.
+
+Options
+=======
+The following options are supported:
+
+* ``iface``: string (**REQUIRED**)
+
+    Network interface name (typically something like ``wlan0``, or ``wlp1s0`` if systemd is used).
+    Run ``ls /sys/class/net`` to list available network interfaces.
+
+* ``timeout``: number
+
+    If specified and not negative, requery information and call ``cb`` every ``timeout`` seconds.
+
+    Note that this is done on any routing/link update anyway, so this is only useful if you want to
+    show the "volatile" properties such as signal level, bitrate, and frequency.
+
+``cb`` argument
+===============
+A table with ``what`` entry.
+
+* If ``what`` is ``"update"``, the information on current wireless connection has been (re)queried,
+  either because it is the first time the plugin does so, a routing/link update has been received,
+  or a timeout has expired.
+  Additionally, the following entries are provided (all are optional):
+
+  - ``ssid``: string
+
+    802.11 network service set identifier (also known as the “network name”).
+
+  - ``signal_percent``: integer
+
+    Signal level, in percents.
+
+  - ``signal_dbm``: number
+
+    Signal level, in dBms.
+    Generally, -90 corresponds to the worst quality, and -20 to the best quality.
+
+  - ``frequency``: number
+
+    Radio frequency, in Hz.
+
+  - ``bitrate``: number
+
+    Bitrate, in units of 100 kbit/s.
+
+* If ``what`` is ``"error"``, no data available for this interface (most likely, an interface with
+  the name specified does not exist).
---
+++ plugins/wireless/wireless_info.c
@@ -0,0 +1,249 @@
+#include "wireless_info.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <net/if.h>
+#include <netlink/netlink.h>
+#include <netlink/genl/genl.h>
+#include <netlink/genl/ctrl.h>
+#include <linux/nl80211.h>
+#include <linux/if_ether.h>
+
+// Adopted from i3status/src/print_wireless_info.c
+
+static
+void
+find_ssid(uint8_t *ies, uint32_t ies_len, uint8_t **ssid, uint32_t *ssid_len)
+{
+    enum { WLAN_EID_SSID = 0 };
+
+    *ssid = NULL;
+    *ssid_len = 0;
+
+    while (ies_len > 2 && ies[0] != WLAN_EID_SSID) {
+        ies_len -= ies[1] + 2;
+        ies += ies[1] + 2;
+    }
+    if (ies_len < (uint32_t) 2 + ies[1]) {
+        return;
+    }
+
+    *ssid_len = ies[1];
+    *ssid = ies + 2;
+}
+
+static
+int
+gwi_sta_cb(struct nl_msg *msg, void *vud)
+{
+    WirelessInfo *info = vud;
+
+    struct nlattr *tb[NL80211_ATTR_MAX + 1];
+    struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
+    struct nlattr *sinfo[NL80211_STA_INFO_MAX + 1];
+    struct nlattr *rinfo[NL80211_RATE_INFO_MAX + 1];
+    struct nla_policy stats_policy[NL80211_STA_INFO_MAX + 1] = {
+        [NL80211_STA_INFO_RX_BITRATE] = {.type = NLA_NESTED},
+    };
+    struct nla_policy rate_policy[NL80211_RATE_INFO_MAX + 1] = {
+        [NL80211_RATE_INFO_BITRATE] = {.type = NLA_U16},
+    };
+
+    if (nla_parse(tb, NL80211_ATTR_MAX, genlmsg_attrdata(gnlh, 0), genlmsg_attrlen(gnlh, 0),
+                  NULL) < 0)
+    {
+        return NL_SKIP;
+    }
+    if (!tb[NL80211_ATTR_STA_INFO]) {
+        return NL_SKIP;
+    }
+
+    if (nla_parse_nested(sinfo, NL80211_STA_INFO_MAX, tb[NL80211_ATTR_STA_INFO],
+                         stats_policy) < 0)
+    {
+        return NL_SKIP;
+    }
+    if (!sinfo[NL80211_STA_INFO_RX_BITRATE]) {
+        return NL_SKIP;
+    }
+
+    if (nla_parse_nested(rinfo, NL80211_RATE_INFO_MAX, sinfo[NL80211_STA_INFO_RX_BITRATE],
+                         rate_policy) < 0)
+    {
+        return NL_SKIP;
+    }
+    if (!rinfo[NL80211_RATE_INFO_BITRATE]) {
+        return NL_SKIP;
+    }
+
+    info->flags |= HAS_BITRATE;
+    info->bitrate = nla_get_u16(rinfo[NL80211_RATE_INFO_BITRATE]);
+
+    return NL_SKIP;
+}
+
+static
+int
+gwi_scan_cb(struct nl_msg *msg, void *vud)
+{
+    WirelessInfo *info = vud;
+    struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
+    struct nlattr *tb[NL80211_ATTR_MAX + 1];
+    struct nlattr *bss[NL80211_BSS_MAX + 1];
+    struct nla_policy bss_policy[NL80211_BSS_MAX + 1] = {
+        [NL80211_BSS_FREQUENCY] = {.type = NLA_U32},
+        [NL80211_BSS_BSSID] = {.type = NLA_UNSPEC},
+        [NL80211_BSS_INFORMATION_ELEMENTS] = {.type = NLA_UNSPEC},
+        [NL80211_BSS_SIGNAL_MBM] = {.type = NLA_U32},
+        [NL80211_BSS_SIGNAL_UNSPEC] = {.type = NLA_U8},
+        [NL80211_BSS_STATUS] = {.type = NLA_U32},
+    };
+
+    if (nla_parse(tb, NL80211_ATTR_MAX, genlmsg_attrdata(gnlh, 0), genlmsg_attrlen(gnlh, 0),
+                  NULL) < 0)
+    {
+        return NL_SKIP;
+    }
+    if (!tb[NL80211_ATTR_BSS]) {
+        return NL_SKIP;
+    }
+
+    if (nla_parse_nested(bss, NL80211_BSS_MAX, tb[NL80211_ATTR_BSS], bss_policy) < 0) {
+        return NL_SKIP;
+    }
+    if (!bss[NL80211_BSS_STATUS]) {
+        return NL_SKIP;
+    }
+
+    const uint32_t status = nla_get_u32(bss[NL80211_BSS_STATUS]);
+
+    if (status != NL80211_BSS_STATUS_ASSOCIATED &&
+        status != NL80211_BSS_STATUS_IBSS_JOINED)
+    {
+        return NL_SKIP;
+    }
+
+    if (!bss[NL80211_BSS_BSSID]) {
+        return NL_SKIP;
+    }
+
+    memcpy(info->bssid, nla_data(bss[NL80211_BSS_BSSID]), ETH_ALEN);
+
+    if (bss[NL80211_BSS_FREQUENCY]) {
+        info->flags |= HAS_FREQUENCY;
+        info->frequency = (double) nla_get_u32(bss[NL80211_BSS_FREQUENCY]) * 1e6;
+    }
+
+    if (bss[NL80211_BSS_SIGNAL_UNSPEC]) {
+        info->flags |= HAS_SIGNAL_PERC;
+        info->signal_perc = nla_get_u8(bss[NL80211_BSS_SIGNAL_UNSPEC]);
+    }
+
+    if (bss[NL80211_BSS_SIGNAL_MBM]) {
+        const int32_t dbm = nla_get_u32(bss[NL80211_BSS_SIGNAL_MBM]) / 100;
+        info->flags |= HAS_SIGNAL_DBM;
+        info->signal_dbm = dbm;
+    }
+
+    if (bss[NL80211_BSS_INFORMATION_ELEMENTS]) {
+        uint8_t *ssid;
+        uint32_t ssid_len;
+        find_ssid(nla_data(bss[NL80211_BSS_INFORMATION_ELEMENTS]),
+                  nla_len(bss[NL80211_BSS_INFORMATION_ELEMENTS]),
+                  &ssid, &ssid_len);
+        if (ssid && ssid_len) {
+            info->flags |= HAS_ESSID;
+            snprintf(info->essid, sizeof(info->essid), "%.*s", ssid_len, ssid);
+        }
+    }
+
+    return NL_SKIP;
+}
+
+bool
+get_wireless_info(const char *iface, WirelessInfo *info)
+{
+    memset(info, 0, sizeof(WirelessInfo));
+    bool ok = false;
+    struct nl_sock *sk = NULL;
+    struct nl_msg *msg = NULL;
+
+    if (!(sk = nl_socket_alloc())) {
+        goto done;
+    }
+    if (genl_connect(sk) != 0) {
+        goto done;
+    }
+
+    if (nl_socket_modify_cb(sk, NL_CB_VALID, NL_CB_CUSTOM, gwi_scan_cb, info) < 0) {
+        goto done;
+    }
+
+    const int nl80211_id = genl_ctrl_resolve(sk, "nl80211");
+    if (nl80211_id < 0) {
+        goto done;
+    }
+
+    const unsigned ifidx = if_nametoindex(iface);
+    if (ifidx == 0) {
+        goto done;
+    }
+
+    if (!(msg = nlmsg_alloc())) {
+        goto done;
+    }
+
+    if (!genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, nl80211_id, 0, NLM_F_DUMP,
+                     NL80211_CMD_GET_SCAN, 0))
+    {
+        goto done;
+    }
+    if (nla_put_u32(msg, NL80211_ATTR_IFINDEX, ifidx) < 0) {
+        goto done;
+    }
+
+    if (nl_send_sync(sk, msg) < 0) {
+        goto done;
+    }
+    // /nl_send_sync()/ calls /nlmsg_free()/
+    msg = NULL;
+
+    if (nl_socket_modify_cb(sk, NL_CB_VALID, NL_CB_CUSTOM, gwi_sta_cb, info) < 0) {
+        goto done;
+    }
+
+    if (!(msg = nlmsg_alloc())) {
+        goto done;
+    }
+
+    if (!genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, nl80211_id, 0, NLM_F_DUMP,
+                     NL80211_CMD_GET_STATION, 0))
+    {
+        goto done;
+    }
+    if (nla_put_u32(msg, NL80211_ATTR_IFINDEX, ifidx) < 0) {
+        goto done;
+    }
+    if (nla_put(msg, NL80211_ATTR_MAC, 6, info->bssid) < 0) {
+        goto done;
+    }
+
+    if (nl_send_sync(sk, msg) < 0) {
+        goto done;
+    }
+    // /nl_send_sync()/ calls /nlmsg_free()/
+    msg = NULL;
+
+    ok = true;
+done:
+    if (msg) {
+        nlmsg_free(msg);
+    }
+    if (sk) {
+        nl_socket_free(sk);
+    }
+    return ok;
+}
