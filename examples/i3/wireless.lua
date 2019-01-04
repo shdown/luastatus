2025631e88b40bffd39729ff3ed02855e51975b6
shdown <shdownnine@gmail.com>
2019-01-04 10:50:05 +0300

    examples/i3/wireless.lua: add

---
+++ examples/i3/wireless.lua
@@ -0,0 +1,32 @@
+local ORIGIN = '•'  -- '●' '○' '◌' '◍' '⬤'
+local RAY = '}'     -- '›' '〉' '❭' '❯' '❱' '⟩' '⟫' '》'
+local MIN_DBM, MAX_DBM = -90, -20
+local NGAUGE = 5
+
+local function round(x)
+    return math.floor(x + 0.5)
+end
+
+local function mk_gauge(dbm)
+    if dbm < MIN_DBM then dbm = MIN_DBM end
+    if dbm > MAX_DBM then dbm = MAX_DBM end
+    local nbright = round(NGAUGE * (dbm - MIN_DBM) / (MAX_DBM - MIN_DBM))
+    return {
+        full_text = ORIGIN .. RAY:rep(nbright) .. '<span color="#709080">' .. RAY:rep(NGAUGE - nbright) .. '</span>',
+        markup = 'pango',
+    }
+end
+
+widget = {
+    plugin = 'wireless',
+    opts = {timeout = 10},
+    cb = function(t)
+        if t.what ~= 'update' then
+            return nil
+        end
+        return {
+            t.ssid and {full_text = t.ssid, color = '#709080'},
+            t.signal_dbm and mk_gauge(t.signal_dbm),
+        }
+    end,
+}
