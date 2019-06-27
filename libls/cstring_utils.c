c8c14eb09fbd3bb34a3bb2e4ecd212a7cd5b20c5
shdown <shdownnine@gmail.com>
2019-06-28 00:26:26 +0300

    libls/cstring_utils.c: only support XSI-compliant strerror_r()
    See: http://man7.org/linux/man-pages/man3/strerror_r.3.html
    
    1. That "(_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !
       _GNU_SOURCE" check is glibc-specific.
    
    2. It is simply wrong. We used "defined(_GNU_SOURCE)" instead of
       "_GNU_SOURCE", as the latter resulted in a compilation error in (the
       most common?) case of "-D_GNU_SOURCE", with all the header files just
       checking for "defined(_GNU_SOURCE)". Like, really, what the fuck?
       They have a totally broken check in the man page.
    
       Turns out, even that way it was slightly wrong: see, for example,
       https://github.com/fmtlib/fmt/issues/93.
    
    3. We *can* ensure that `libls/cstring_utils.c` file is compiled with
       the feature test macros that result in the XSI-compliant version of
       strerror_r().

--- libls/cstring_utils.c
+++ libls/cstring_utils.c
@@ -1,15 +1,14 @@
 #include "cstring_utils.h"
 
-#include <string.h>
+#if _POSIX_C_SOURCE < 200112L || defined(_GNU_SOURCE)
+#   error "Unsupported feature test macros; either tune them or change the code."
+#endif
 
 const char *
 ls_strerror_r(int errnum, char *buf, size_t nbuf)
 {
-#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! defined(_GNU_SOURCE)
-    // XSI-compliant /strerror_r/
-    return strerror_r(errnum, buf, nbuf) == 0 ? buf : "unknown error";
-#else
-    // GNU-specific /strerror_r/
-    return strerror_r(errnum, buf, nbuf);
-#endif
+    // We introduce an /int/ variable in order to get a compilation warning if /strerror_r()/ is
+    // still GNU-specific and returns a pointer to char.
+    const int r = strerror_r(errnum, buf, nbuf);
+    return r == 0 ? buf : "unknown error or truncated error message";
 }
