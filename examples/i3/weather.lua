74cea4fdd6ed7bfca4bb916faa24e827033302c9
Layerex <59440032+Layerex@users.noreply.github.com>
2020-06-18 18:22:50 +0000

    Add HTTPS support

--- examples/i3/weather.lua
+++ examples/i3/weather.lua
@@ -1,8 +1,41 @@
 -- you need to install 'lua-curl' and 'gonapps-url-encoder' modules (e.g. with luarocks) 
 -- you can look up all available flags here: https://github.com/chubin/wttr.in#one-line-output
 
-local curl = require('cURL')
-local encoder = require('gonapps.url.encoder')
+local https = require('ssl.https')
+local ltn12 = require('ltn12')
+
+-- All the arguments except for 'url' may be absent or nil; default method is GET.
+-- Returns: code (integer), body (string), headers (table), status (string).
+function request(url, headers, method, body)
+    local out_body = {}
+    local is_ok, code_or_errmsg, out_headers, status = https.request(
+        {
+            url = url,
+            sink = ltn12.sink.table(out_body),
+            redirect = false,
+            cafile = '/etc/ssl/certs/ca-certificates.crt',
+            verify = 'peer',
+            method = method,
+            headers = headers,
+        },
+        body)
+    assert(is_ok, code_or_errmsg)
+    return code_or_errmsg, table.concat(out_body), out_headers, status
+end
+
+-- Arguments are the same to those of 'request'.
+-- Returns: body (string), headers (table).
+function request_check_code(...)
+    local code, body, headers, status = request(...)
+    assert(code == 200, string.format('HTTP %s %s', code, status))
+    return body, headers
+end
+
+function urlencode(s)
+    return string.gsub(s, '[^-_.~a-zA-Z0-9]', function(c)
+        return string.format('%%%02X', string.byte(c))
+    end)
+end
 
 local base_url = 'wttr.in'
 local lang = 'en'
@@ -11,21 +44,25 @@ local PERIOD = 900
 
 function get_weather(format)
     -- encoding is needed to allow usage of special use characters
-    format = encoder.encode(format)
-    curl.easy{
-        url = string.format('%s.%s/%s?format=%s', lang, base_url, location, format),
-        writefunction = function (str)
-            res = str:gsub("\n","")
-        end
-    }:perform()
-    :close()
-    return res
+    format = urlencode(format)
+    url = string.format('https://%s.%s/%s?format=%s', lang, base_url, location, format)
+    local is_ok, body = pcall(request_check_code, url)
+    if is_ok then
+        return body:gsub("\n", "")
+    else
+        return nil
+    end
 end
 
 widget = {
     plugin = 'timer',
     opts = {period = PERIOD},
     cb = function()
-        return {full_text = get_weather('%l: %C %t(%f)')}
+        text = get_weather('%l: %C %t(%f)')
+        if text == nil then
+            luastatus.plugin.push_period(60) -- retry in 60 seconds
+            text = '......'
+        end
+        return {full_text = text}
     end,
 }
