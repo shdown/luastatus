1cf3bdc13f3916bcfec24902041e5037d2185f42
shdown <shdownnine@gmail.com>
2020-07-04 02:15:46 +0300

    release.sh: update

--- release.sh
+++ release.sh
@@ -49,21 +49,21 @@ release_msg="Release $NEW_VERSION"
 if [ "$last_msg" = "$release_msg" ]; then
     echo ""
     echo "Last commit message is “$last_msg”."
-    echo "Apparently, you ran this script before, and “git tag”/“git push” had failed."
-    ask 'Jump to the “git tag” part?'
-else
-    T git merge $DEV_BRANCH
-    T_redir_to_file VERSION echo $NEW_VERSION
-    T_append_to_file RELEASE_NOTES echo
-    T sed \
-        -e "1i\luastatus $NEW_VERSION ($(LC_ALL=C date +'%B %d, %Y'))\n===\n(Please describe the changes here.)\n" \
-        -e :a -e '/^\n*$/{$d;N;};/\n$/ba' \
-        -i RELEASE_NOTES
-    T ${EDITOR:-edit} RELEASE_NOTES
-    ask 'Commit?'
-    T git add VERSION RELEASE_NOTES
-    T git commit -m "$release_msg"
+    echo "Apparently, you have run this script before, so exiting now."
+    exit 1
 fi
+
+T git merge $DEV_BRANCH
+T_redir_to_file VERSION echo $NEW_VERSION
+T_append_to_file RELEASE_NOTES echo
+T sed \
+    -e "1i\luastatus $NEW_VERSION ($(LC_ALL=C date +'%B %d, %Y'))\n===\n(Please describe the changes here.)\n" \
+    -e :a -e '/^\n*$/{$d;N;};/\n$/ba' \
+    -i RELEASE_NOTES
+T ${EDITOR:-edit} RELEASE_NOTES
+ask 'Commit?'
+T git add VERSION RELEASE_NOTES
+T git commit -m "$release_msg"
 
 cat <<EOF
 
