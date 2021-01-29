bd76a0babab091ca2111a9c854a2c07243035aea
shdown <shdownnine@gmail.com>
2021-01-29 20:21:00 +0300

    tests/mpd_tests/mpd/: don't use accept-timeout socat option
    It's too new for travis-ci.

--- tests/pt_tests/mpd/00-common.lib.bash
+++ tests/pt_tests/mpd/00-common.lib.bash
@@ -16,21 +16,6 @@ local function _fmt_kv(m)
 end
 '
 
-mpd_check_port() {
-    echo >&2 "[mpd] Checking port $port..."
-    socat - "TCP-LISTEN:$port,bind=localhost,accept-timeout=0.1"
-}
-
-while ! mpd_check_port; do
-    echo >&2 "[mpd] Port $port does not seem to be free; incrementing."
-    (( ++port ))
-    if (( port >= 65536 )); then
-        pt_fail "[mpd] Cannot choose port."
-    fi
-done
-
-echo >&2 "[mpd] Chosen port: $port."
-
 mpd_wait_for_socat() {
     sleep 1
 }
