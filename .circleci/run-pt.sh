efa03b3d1bc58cc30549ffcad97f585348c7318c
shdown <shdownnine@gmail.com>
2021-02-06 22:30:12 +0300

    .circleci/run-pt.sh: update

--- .circleci/run-pt.sh
+++ .circleci/run-pt.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-sudo ./.circleci/mini-systemd.sh init
-sudo ./.circleci/mini-systemd.sh launch pa pulseaudio --daemonize=no
+./.circleci/mini-systemd.sh init
+./.circleci/mini-systemd.sh launch pa pulseaudio --daemonize=no
 
-PT_TOOL=valgrind PT_MAX_LAG=200 ./tests/pt.sh . skip:plugin-dbus
+c=0
+PT_TOOL=valgrind PT_MAX_LAG=200 ./tests/pt.sh . skip:plugin-dbus || c=$?
 
-sudo ./.circleci/mini-systemd.sh kill-everything
+./.circleci/mini-systemd.sh kill-everything
+
+exit "$c"
