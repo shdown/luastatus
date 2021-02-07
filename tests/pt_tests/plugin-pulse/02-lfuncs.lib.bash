57d9a4209980dcdf4c1281839ce46669f3e5f5e6
shdown <shdownnine@gmail.com>
2021-02-07 19:15:34 +0300

    Another test for plugin-pulse

--- tests/pt_tests/plugin-pulse/02-lfuncs.lib.bash
+++ tests/pt_tests/plugin-pulse/02-lfuncs.lib.bash
@@ -16,9 +16,13 @@ widget = {
     plugin = '$PT_BUILD_DIR/plugins/pulse/plugin-pulse.so',
     opts = {
         sink = '$sink_name',
+        make_self_pipe = true,
     },
     cb = function(t)
-        assert(t)
+        if t == nil then
+            f:write('cb nil\n')
+            return
+        end
         local line = string.format('cb %.0f%%', t.cur / t.norm * 100)
         if t.mute then
             line = line .. ' (mute)'
@@ -27,22 +31,28 @@ widget = {
             f:write(line .. '\n')
         end
         last_line = line
+        luastatus.plugin.wake_up()
     end,
 }
 __EOF__
 pt_spawn_luastatus
 exec {pfd}<"$main_fifo_file"
 pt_expect_line 'init' <&$pfd
+
 pt_expect_line 'cb 100%' <&$pfd
+pt_expect_line 'cb nil' <&$pfd
 
 pacmd set-sink-volume "$sink_name" 32768
 pt_expect_line 'cb 50%' <&$pfd
+pt_expect_line 'cb nil' <&$pfd
 
 pacmd set-sink-mute "$sink_name" true
 pt_expect_line 'cb 50% (mute)' <&$pfd
+pt_expect_line 'cb nil' <&$pfd
 
 pacmd set-sink-volume "$sink_name" 2048
 pt_expect_line 'cb 3% (mute)' <&$pfd
+pt_expect_line 'cb nil' <&$pfd
 
 pt_close_fd "$pfd"
 pt_testcase_end
