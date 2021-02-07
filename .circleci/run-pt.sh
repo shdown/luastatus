54f2af3fa600f677a74e0dc1d34c946bc8b6655d
shdown <shdownnine@gmail.com>
2021-02-07 16:13:15 +0300

    .circleci/: another try for D-Bus

--- .circleci/run-pt.sh
+++ .circleci/run-pt.sh
@@ -20,22 +20,32 @@ pa_start() {
     done
 }
 
+pa_end() {
+    pulseaudio --kill || true
+}
+
 dbus_start() {
     DBUS_DAEMON_PID=
     atexit+=(dbus_end)
 
-    dbus-daemon --session --nofork --nopidfile &
+    local addr_file=/tmp/dbus-session-addr.txt
+
+    dbus-daemon --session --nofork --print-address=3 3>"$addr_file" &
     DBUS_DAEMON_PID=$!
+
+    local x
+    while ! IFS= read -r x < "$addr_file"; do
+        echo >&2 "Waiting for D-Bus daemon..."
+        sleep 1
+    done
+    rm -f "$addr_file"
+    export DBUS_SESSION_BUS_ADDRESS="$x"
 }
 
 dbus_end() {
     if [[ -n "$DBUS_DAEMON_PID" ]]; then
         kill "$DBUS_DAEMON_PID" || true
     fi
-}
-
-pa_end() {
-    pulseaudio --kill || true
 }
 
 pa_start
