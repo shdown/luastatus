8623175aaf3ab4dd21b9474ad14a2d7473ce8d98
shdown <shdownnine@gmail.com>
2021-02-07 15:41:12 +0300

    .circleci/: try to run dbus tests

--- .circleci/run-pt.sh
+++ .circleci/run-pt.sh
@@ -20,10 +20,25 @@ pa_start() {
     done
 }
 
+dbus_start() {
+    DBUS_DAEMON_PID=
+    atexit+=(dbus_end)
+
+    dbus-daemon --session --nofork --nopidfile &
+    DBUS_DAEMON_PID=$!
+}
+
+dbus_end() {
+    if [[ -n "$DBUS_DAEMON_PID" ]]; then
+        kill "$DBUS_DAEMON_PID" || true
+    fi
+}
+
 pa_end() {
     pulseaudio --kill || true
 }
 
 pa_start
+dbus_start
 
-PT_TOOL=valgrind PT_MAX_LAG=250 ./tests/pt.sh . skip:plugin-dbus
+PT_TOOL=valgrind PT_MAX_LAG=250 ./tests/pt.sh .
